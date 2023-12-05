9d1d3082993a656a14fe7d63ccef0a57fcd1f629
alicebyers5 <alice.byers@gov.scot>
2023-12-05 14:29:17 +0000

    Display DQ text from data file

--- ui.R
+++ ui.R
@@ -6,14 +6,27 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Example app - automated data quality text"),
 
+    # Dynamic DQ text
     selectInput("board",
-                    "Select a Board",
-                    choices = c("Scotland",
-                                "Ayrshire & Arran",
-                                "Borders",
-                                "Dumfries & Galloway")
+                "Select a Board",
+                choices = c("Scotland",
+                            "Ayrshire & Arran",
+                            "Borders",
+                            "Dumfries & Galloway")
     ),
     
-    uiOutput("dq_text")
+    uiOutput("dq_text"),
+    
+    br(),
+    hr(),
+
+    # Notes page example
+    map(
+        unique(dq$board),
+        ~ tagList(
+            h2(.x),
+            map(dq %>% filter(board == .x) %>% pull(text), p)
+        )
+    )
     
 ))
